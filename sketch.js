f4d70459205f40d9203f4b6a7193c99dec684dde
Jake Zweifler <jake.zweifler@gmail.com>
2024-08-16 14:06:41 -0400

    added js

--- sketch.js
+++ sketch.js
@@ -1,56 +1,68 @@
-let outerCircle;
-let circles = [];
+let sliderAng;
+
+
+function cos_sum(n, T) {
+  let sum = 0;
+  for(let i = 1; i <= n; i += 1) {
+    sum += cos(i*(PI - T));
+  }
+  return sum;
+  
+//   let c1 = sin(n*(PI - T)/2);
+//   let c2 = sin((PI - T)/2);
+//   let c3 = cos((n+1)*(PI - T)/2);
+          
+//   return c1*c2/c3;
+
+}
+
+function sin_sum(n, T) {
+  let sum = 0;
+  for(let i = 1; i <= n; i += 1) {
+    sum += sin(i*(PI - T));
+  }
+  return sum;
+  
+//   let c1 = sin(n * (PI - T) / 2);
+//   let c2 = sin((PI - T)/2);
+//   let c3 = sin((n+1)*(PI - T) / 2);
+  
+//   console.log(c1);
+//   console.log(c2);
+//   console.log(c3);
+  
+//   return c1*c2/c3;
+}
+
 
 function setup() {
-  createCanvas(600, 600);
-
-  outerCircle = { x: width / 2, y: height / 2, radius: min(width, height) * 0.4 };
+  createCanvas(1200, 600);
+  sliderAng = createSlider(185, 360, 0, 5);
+  sliderAng.position(10, 10);
+  sliderAng.style('width', '180px');
   
-  // Randomly generate radii for the first two circles
-  let r1 = random(outerCircle.radius / 5, outerCircle.radius / 3);
-  let r2 = random(outerCircle.radius / 5, outerCircle.radius / 3);
   
-  // Calculate radius of the third circle using Descartes' Circle Theorem
-  let k1 = 1 / r1;
-  let k2 = 1 / r2;
-  let k3 = k1 + k2 + 2 * sqrt(k1 * k2); // Curvature of third circle
-  let r3 = 1 / k3;
-  
-  // Position the circles inside the outer circle
-  let circle1 = { x: outerCircle.x - r1, y: outerCircle.y, radius: r1 };
-  let circle2 = { x: outerCircle.x + r1 + r2, y: outerCircle.y, radius: r2 };
-  let circle3 = { x: outerCircle.x, y: outerCircle.y + r1 + r3, radius: r3 };
-  
-  circles.push(circle1, circle2, circle3);
+ 
 }
 
 function draw() {
-  background(200);
 
-
-
-
-  // Draw a simple circle in the center of the canvas
-  noFill(); // No fill color
-  stroke(0); // Black stroke
-  strokeWeight(2); // Thickness of the line
-  ellipse(width / 2, height / 2, 300, 300); // Draw the circle
-
-
-
-
-//   // Draw the outer circle
-//   noFill();
-//   stroke(0);
-//   strokeWeight(2);
-//   ellipse(outerCircle.x, outerCircle.y, outerCircle.radius * 2);
   
-//   // Draw the three mutually tangent circles
-//   for (let i = 0; i < circles.length; i++) {
-//     let c = circles[i];
-//     ellipse(c.x, c.y, c.radius * 2);
-//   }
+  background(200);
+  
+  let T = PI*sliderAng.value()/180;
+
+  
+  translate(width / 2, height / 1.5);
+
+  let s = width/7;
+  
+  
+  for (let n = 0; n < 200; n += 1) {
+    
+    
+    line(s*cos_sum(n, T), s*sin_sum(n,T), 
+         s*cos_sum(n+1, T), s*sin_sum(n+1,T));
+    
+  }
 }
-
-
-
